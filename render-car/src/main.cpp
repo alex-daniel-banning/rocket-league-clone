03f37f3e520e23d9bc89dd3d91dc736d62796e7f
Alex Banning <banninghg80@gmail.com>
2025-10-23 21:01:40 -0500

    shader basics/uniforms

--- render-car/src/main.cpp
+++ render-car/src/main.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <cmath>
 
 float vertices[] = {
      0.5f,  0.5f, 0.0f,  // top right
@@ -15,16 +16,18 @@ unsigned int indices[] = {  // note that we start from 0!
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
     "}\0";
 const char *fragmentShaderSource ="#version 330 core\n"
 	"out vec4 FragColor;\n"
-	"\n"
+	"uniform vec4 ourColor;\n"
 	"void main()\n"
 	"{\n"
-	"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"    FragColor = ourColor;\n"
 	"}\0";
 
 int main() {
@@ -127,6 +130,12 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
+
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
