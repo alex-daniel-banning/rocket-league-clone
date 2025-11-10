e165fd09bdaf0e9b5c3022d8a4c53ba330f296cb
Alex Banning <banninghg80@gmail.com>
2025-11-09 21:30:46 -0600

    add specular lighting (shiny reflection)

--- render-car/src/main.cpp
+++ render-car/src/main.cpp
@@ -149,6 +149,7 @@ int main()
         lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
         lightingShader.setVec3("lightPos", lightPos.x, lightPos.y, lightPos.z);
+        lightingShader.setVec3("viewPos", camera.Position.x, camera.Position.y, camera.Position.z);
         glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 view;
         view = camera.GetViewMatrix();
