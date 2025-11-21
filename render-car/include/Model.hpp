ebbd87256e70b92c0c746b2dcd34d67b6bcc62d3
Alex Banning <banninghg80@gmail.com>
2025-11-20 23:47:52 -0600

    add model loading code, compiles, but haven't tested yet

---
+++ render-car/include/Model.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <assimp/material.h>
+#include <assimp/scene.h>
+#include <vector>
+#include "Mesh.hpp"
+#include "Shader.hpp"
+
+unsigned int TextureFromFile(const char *path, const std::string &directory, bool gamma = false);
+
+class Model
+{
+  public:
+    Model(char *path) { loadModel(path); }
+    void Draw(Shader &shader);
+
+  private:
+    // model data
+    std::vector<Mesh> meshes;
+    std::string directory;
+    std::vector<Texture> textures_loaded;
+
+    void loadModel(std::string path);
+    void processNode(aiNode *node, const aiScene *scene);
+    Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type,
+                                              std::string typeName);
+};
