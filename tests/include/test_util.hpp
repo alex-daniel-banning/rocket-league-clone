fae29ac85136eeaac31ae45759545db7a1cd4676
Alex Banning <banninghg80@gmail.com>
2026-01-25 13:03:38 -0600

    more formatting stuff

--- tests/include/test_util.hpp
+++ tests/include/test_util.hpp
@@ -2,12 +2,12 @@
 #include <glm/vec3.hpp>
 #include <string>
 
-class test_util
-{
-  public:
-    static bool Vec3Near(const glm::vec3 &expected, const glm::vec3 &actual, float eps = 1e-5f);
-    static void ExpectVec3Near(const glm::vec3 &expected, const glm::vec3 &actual,
-                               std::string msg = "", float eps = 1e-5f);
-    static void AssertVec3Near(const glm::vec3 &expected, const glm::vec3 &actual,
-                               std::string msg = "", float eps = 1e-5f);
+class test_util {
+ public:
+  static bool Vec3Near(const glm::vec3& expected, const glm::vec3& actual,
+                       float eps = 1e-5f);
+  static void ExpectVec3Near(const glm::vec3& expected, const glm::vec3& actual,
+                             std::string msg = "", float eps = 1e-5f);
+  static void AssertVec3Near(const glm::vec3& expected, const glm::vec3& actual,
+                             std::string msg = "", float eps = 1e-5f);
 };
